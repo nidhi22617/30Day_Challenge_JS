9fbcd24332519a707d654e58b9a009020abf0194
nidhi22617 <pnidhi2@hotmail.com>
2024-07-17 23:57:02 +0545

    Day 1 Complete

---
+++ Day2_Operators.js
@@ -0,0 +1,17 @@
+// Activity 1 : Arithmetic Operators
+    //Task 1 : Write a program to add two numbers and log the result to the console
+        let num1 = 6
+        let num2 =5
+        // console.log(num1 + num2);
+        // console.log(num1 - num2);
+        // console.log(num1 * num2);
+        // console.log(num1 / num2);
+        // console.log(num1 % num2);
+
+// Activity 2 : Assignment Operators
+        console.log(num1 += 3);
+        console.log(num1 -= 2);
+// Activity 3 : Comparison Operators
+
+
+
---
+++ Day_1_Varriables.js
@@ -0,0 +1,56 @@
+// Activity-1 Varriable Declaration
+// Task 1
+// var num1 = 7
+// console.log(num1);
+//Task 2
+let MyName = "Premendea"
+// console.log(MyName); 
+
+// Activity-2 Constant Declaration
+// Task 3
+ const isLogin = true
+//  console.log(isLogin);
+
+//  isLogin = false  // produce error : Assignment to constant variable
+
+// Activity-3 Data Types
+    // Task 4
+        // let rollNumber = 10
+        // let fName = "Premendra"
+        // let isStudent = false
+        // console.log(`Roll Number : ${rollNumber}`);
+        // console.log(`First Name : ${fName}`);
+        // console.log(`is Student : ${isStudent}`);
+
+// Object
+    // const student = {
+    //     fName : "premendra",
+    //     age : 55,
+    //     education : "B.Ed."
+    // }
+    // console.log(student);
+//Array
+    const marks = [22,23,24,25,23,1,24]
+    // console.log(marks);
+// Activity-4 Data Types
+    // Task 5
+        let num1 = 5
+        const num2 = 8
+        console.log(num1);
+        console.log(num2);
+        num1 = 9
+        console.log(num1);
+        num2 = 6
+        console.log(num2);
+
+
+
+
+
+
+
+
+
+// var num2 = 8
+// var result = num1 + num2
+// console.log(`The Sum of num1${num1} and num2 ${num2} is :${result}`);
