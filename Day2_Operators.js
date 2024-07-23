144574bb55acb12c2cc5f049d4cb2b17c5da77f2
nidhi22617 <pnidhi2@hotmail.com>
2024-07-23 09:51:32 +0545

    day 3 file created

--- Day2_Operators.js
+++ Day2_Operators.js
@@ -1,6 +1,6 @@
 // Activity 1 : Arithmetic Operators
     //Task 1 : Write a program to add two numbers and log the result to the console
-        let num1 = 6
+        let num1 = '5'
         let num2 =5
         // console.log(num1 + num2);
         // console.log(num1 - num2);
@@ -9,9 +9,26 @@
         // console.log(num1 % num2);
 
 // Activity 2 : Assignment Operators
-        console.log(num1 += 3);
-        console.log(num1 -= 2);
+        // console.log(num1 += 3);
+        // console.log(num1 -= 2);
 // Activity 3 : Comparison Operators
+        // console.log(num1 > num2);
+        // console.log(num1 < num2);
+        // console.log(num1 >= num2);
+        // console.log(num1 <= num2);
+        // console.log(num1 == num2);
+        // console.log(num1 === num2);
+// Activity 4: Logical Operators
+        const num3 = 6
+        const num4 = 8
+        // console.log(num3 > 7 && num4< 9);
+        // console.log(num3 > 7 || num4< 9);
+        // console.log(num3 != 6 || num4< 9);
+        // console.log(num3 != 6 && num4< 9);
+// Activity 5: Ternery Operator
+        const num5 = 9
+
+
 
 
 
---
+++ Day3_ControlStatements.js
@@ -0,0 +1,3 @@
+let num1 = 5
+let num2 = 9
+console.log(`The value of Num1 is :${num1}`);
